06e05b7636942a63e16d12c4bb8c8000622a34b3
nkostopoulos <nkostopoulos@netmode.ntua.gr>
2019-03-12 01:12:49 +0200

    files for lab1

---
+++ lab1/demo3.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+
