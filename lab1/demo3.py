d08610734068bba8f5dbc11b493ba88328c951e3
Nikos Kostopoulos <33379131+nkostopoulos@users.noreply.github.com>
2019-03-12 12:42:53 +0200

    added helpful lines and comments

--- lab1/demo3.py
+++ lab1/demo3.py
@@ -4,3 +4,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.model_selection import train_test_split, cross_val_score
 
+# load csv file
+df  = ___
+
+# split dataset
+X_train_raw, X_test_raw, ____, ____ = train_test_split(______)
+
+# vectorize data
+vectorizer = ________
+Xtrain = vectorizer.fit_transform(_______)
+
+# fit Logistic Regression model
+____
+____
+
+# calculate accurary based on test set
+_________________________
