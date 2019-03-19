049c0e4e4dc31a7d392da0a9ac7c6d8cda4cfcf8
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2019-03-19 17:06:48 +0200

    Update datak.py

--- lab2/datak.py
+++ lab2/datak.py
@@ -1,9 +1,9 @@
-from pylab import plot,show
 from numpy import vstack,array
 from numpy.random import rand
 import numpy as np
 from scipy.cluster.vq import kmeans,vq
 import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader as dr
 from math import sqrt
 from sklearn.cluster import KMeans
@@ -14,7 +14,7 @@ sp500_url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
  
 #read in the url and scrape ticker data
 data_table = pd.read_html(sp500_url)
- 
+
 tickers = data_table[0][1:][0].tolist()
 prices_list = []
 for ticker in tickers:
@@ -30,3 +30,24 @@ for ticker in tickers:
 prices_df.sort_index(inplace=True)
  
 prices_df.head()
+
+#Calculate average annual percentage return and volatilities over a theoretical one year period
+returns = prices_df.pct_change().mean() * 252
+returns = pd.DataFrame(returns)
+returns.columns = ['Returns']
+returns['Volatility'] = prices_df.pct_change().std() * sqrt(252)
+
+#format the data as a numpy array to feed into the K-Means algorithm
+data = np.asarray([np.asarray(returns['Returns']),np.asarray(returns['Volatility'])]).T
+
+X = data
+distorsions = []
+for k in range(2, 20):
+    k_means = KMeans(n_clusters=k)
+    k_means.fit(X)
+    distorsions.append(k_means.inertia_)
+
+fig = plt.figure(figsize=(15, 5))
+plt.plot(range(2, 20), distorsions)
+plt.grid(True)
+plt.title('Elbow curve')
