cb2bb0e6d682eeb02073a495c985cef2f29415ee
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2019-03-20 11:34:44 +0200

    Update demo2.py

--- lab2/demo2.py
+++ lab2/demo2.py
@@ -6,9 +6,10 @@ import sklearn.metrics as sm
 import pandas as pd
 import numpy as np
 
-# import some data to play with
+# import some data to play with (from sklearn)
 iris = datasets.load_iris()
 
+#print is optional
 print iris.data
 iris.feature_names
 print iris.target
